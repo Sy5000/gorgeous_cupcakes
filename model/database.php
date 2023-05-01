65ec84c27cd697d04492d1278e8d6a802862a221
Sy5000 <sy.d@hotmail.co.uk>
2023-05-01 12:47:26 +1000

    pdo update

--- model/database.php
+++ model/database.php
@@ -1,11 +1,19 @@
 <?php
 // git update
 	//database connection details
-	$host = 'gcdatabase.cn4uuyf6npxw.us-east-1.rds.amazonaws.com';
-	$user = 'admin';
-	$password = 'Password123';
-	$database = 'gcdatabase';
-
+	// $host = 'gcdatabase.cn4uuyf6npxw.us-east-1.rds.amazonaws.com/3306';
+	// $user = 'admin';
+	// $password = 'Password123';
+	// $database = 'gcdatabase';
+	$dbhost = $_SERVER['RDS_HOSTNAME'];
+	$dbport = $_SERVER['RDS_PORT'];
+	$dbname = $_SERVER['RDS_DB_NAME'];
+	$charset = 'utf8' ;
+	
+	$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+	$username = $_SERVER['RDS_USERNAME'];
+	$password = $_SERVER['RDS_PASSWORD'];
+	
 	//connect to database with a try/catch statement
 	//if the connection is not successful display the error message via database_error.php
 	//the PDOException class represents the error raised by PDO
@@ -13,7 +21,8 @@
 	//the PDO error is returned as a string via the getMessage() function
 	try
 	{
-		$conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+		$conn = new PDO($dsn, $username, $password);
+		// $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
 	}
 	catch(PDOException $e)
 	{
